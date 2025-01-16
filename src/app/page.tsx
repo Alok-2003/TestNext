093786769a9d2aa27879672d19f2b809e77ce17f
Alok-2003 <alok953280@gmail.com>
2025-01-16 14:53:10 +0530

    check1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,24 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <Navbar/>
-        <Link href={'/home'} >
-        <h1>Home route</h1>
-        </Link>
-      </div>
+      <div className="bg-gray-900 text-white min-h-screen">
+      <Navbar/>
+      <section className="flex h-screen flex-col items-center justify-center text-center py-20 bg-gradient-to-r from-blue-500 to-green-400">
+        <h2 className="text-4xl md:text-6xl font-bold mb-6">Trade Smarter with Bit Bazaar</h2>
+        <p className="text-xl md:text-2xl mb-8 max-w-3xl">
+          Your one-stop solution for secure, fast, and reliable cryptocurrency trading.
+        </p>
+        <div>
+          <a
+            href="/home"
+            className="bg-white text-gray-900 px-6 py-3 rounded-lg font-semibold shadow-lg hover:bg-gray-200"
+          >
+            Get Started
+          </a>
+        </div>
+      </section>
+
+    </div>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+'use client'
 
-const Navbar = () => {
+import Link from 'next/link'
+
+export default function Navbar() {
+
+  // const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=2';
+  // const options = {
+  //   method: 'GET',
+  //   headers: { accept: 'application/json', 'x-cg-demo-api-key': 'CG-ekwns8dzHHTY21EnHqgZE8t1' }
+  // };
+
+  // fetch(url, options)
+  //   .then(res => res.json())
+  //   .then(json => console.log(json))
+  //   .catch(err => console.error(err));
   return (
-    <div className='text-bold text-centre text-4xl font-bold' >Navbar</div>
+    <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-md">
+      <div className="flex items-center">
+        <Link href={'/'} >
+        <h1 className="text-2xl font-bold font-mono text-black ">Bit Bazaar</h1>
+        </Link>
+      </div>
+
+      <div className="flex items-center space-x-6">
+        {/* Navigation links */}
+        <Link href="/" className="text-black font-medium hover:text-gray-800 transition-colors">
+          Crypto Taxes
+        </Link>
+        <Link href="/" className="text-black font-medium hover:text-gray-800 transition-colors">
+          Free Tools
+        </Link>
+        <Link href="/" className="text-black font-medium hover:text-gray-800 transition-colors">
+          Resource Centre
+        </Link>
+
+      </div>
+    </nav>
   )
 }
-
-export default Navbar
